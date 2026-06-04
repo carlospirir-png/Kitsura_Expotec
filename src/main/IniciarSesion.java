ec96ca4c4c8dd0afecaaa88454fc32056a3b8644
Mario <mariotumax@emilianisomascos.edu.gt>
2026-06-04 11:16:29 -0600

    Reorganizacion de carpetas

---
+++ src/main/IniciarSesion.java
@@ -0,0 +1,77 @@
+// INICIAR SESIOOOOOOOOOOOOOOOON
+package main;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class IniciarSesion extends JFrame {
+    private FondoPanel fondo;
+    
+     public IniciarSesion () {
+        fondo = new FondoPanel("/utiles/fondoUnoK.png");
+        setContentPane(fondo);
+        setTitle("Inicio de sesión");
+        setSize(1880,1080);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        fondo.setLayout(null);
+        crearComponentes();
+        setVisible(true);
+    }
+     private void crearComponentes() {
+        //---------------- L O G O ----------------
+        JLabel logo = new JLabel();
+        ImageIcon logoIcon = new ImageIcon(getClass().getResource("/main/logofK.png"));
+        Image logoEscalado = logoIcon.getImage().getScaledInstance(150,150,Image.SCALE_SMOOTH );
+        logo.setIcon(new ImageIcon(logoEscalado));
+        logo.setBounds(900,40,150,150);
+        fondo.add(logo);
+        
+        //---------------- MASCOTA ----------------
+        JLabel mascota = new JLabel();
+        ImageIcon mascotaIcon = new ImageIcon(getClass().getResource("/main/mascota1.png"));
+        Image mascotaEscalada = mascotaIcon.getImage().getScaledInstance(191,264,Image.SCALE_SMOOTH);
+        mascota.setIcon(new ImageIcon(mascotaEscalada));
+        mascota.setBounds(1250,550,191,264);
+        fondo.add(mascota);
+        
+        //---------------- ESLOGAN ----------------
+        JLabel titulo = new JLabel("No es magia, es mente ");
+        titulo.setFont(new Font("Arial", Font.BOLD, 20));
+        titulo.setForeground(Color.WHITE);
+        titulo.setBounds(865,190,400,60);
+        fondo.add(titulo);
+        
+        //---------------- USUARIO ----------------
+        JLabel lblUsuario = new JLabel("NOMBRE: ");
+        lblUsuario.setFont(new Font("Arial", Font.BOLD, 18));
+        lblUsuario.setForeground(Color.WHITE);
+        lblUsuario.setBounds(760,350,200,30);
+        fondo.add(lblUsuario);
+        
+        JTextField txtUsuario = new JTextField();
+        txtUsuario.setBounds(760,385,400,50);
+        fondo.add(txtUsuario);
+        
+        //---------------- PASSWORD ----------------
+        JLabel lblPassword = new JLabel("CONTRASEÑA");
+        lblPassword.setFont(new Font("Arial", Font.BOLD, 18));
+        lblPassword.setForeground(Color.WHITE);
+        lblPassword.setBounds(760,450,200,30);
+        fondo.add(lblPassword);
+        JPasswordField txtPassword = new JPasswordField();
+        txtPassword.setBounds(760,485,400,50);
+        fondo.add(txtPassword);
+
+        //---------------- BOTON ----------------
+        JButton btnLogIn = new JButton("INICIAR SESION");
+        btnLogIn.setBounds(820,600,285,60);
+        fondo.add(btnLogIn);
+    }
+
+    public static void main(String[] args) {
+
+        new IniciarSesion();
+}
+}
---
+++ src/main/MenuPrincipal.java
@@ -0,0 +1,76 @@
+
+package main;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class MenuPrincipal extends JFrame {
+    private FondoPanel fondo;
+    
+    public MenuPrincipal (){
+        fondo = new FondoPanel("/utiles/fondoDosK.png");
+        setContentPane(fondo);
+        setTitle("Página Principal");
+        setSize(1880,1080);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        fondo.setLayout(null);
+        crearComponentes();
+        setVisible(true);
+    }
+    private void crearComponentes(){
+        //---------------- T I T U L O ----------------
+        JLabel lblTitulo = new JLabel("MENÚ PRINCIPAL");
+        lblTitulo.setFont(new Font("Arial", Font.BOLD, 36));
+        lblTitulo.setForeground(Color.WHITE);
+        lblTitulo.setBounds(810, 120, 500, 50);
+        fondo.add(lblTitulo);
+        
+        //---------------- BOTON PERFIL ----------------
+        JButton btnPerfil = new JButton("PERFIL");
+        btnPerfil.setFont(new Font("Arial", Font.BOLD, 16));
+        btnPerfil.setBounds(820, 320, 285, 65);    
+        btnPerfil.setIconTextGap(5);  
+        ImageIcon icoPerfil = new ImageIcon(getClass().getResource("/utiles/pencil.png"));
+        Image imgPerfil = icoPerfil.getImage().getScaledInstance(35,35, Image.SCALE_SMOOTH);
+        btnPerfil.setIcon(new ImageIcon(imgPerfil));        
+        fondo.add(btnPerfil);
+        
+        //---------------- BOTON MINIJUEGOS ----------------
+        JButton btnMinijuegos = new JButton("MINIJUEGOS");
+        btnMinijuegos.setFont(new Font("Arial", Font.BOLD, 16));
+        btnMinijuegos.setBounds(820, 440, 285, 65);
+        btnMinijuegos.setIconTextGap(5);        
+        ImageIcon icoMinijuegos = new ImageIcon(getClass().getResource("/utiles/mandoo.png"));
+        Image imgMinijuegos = icoMinijuegos.getImage().getScaledInstance(35, 35, Image.SCALE_SMOOTH);
+        btnMinijuegos.setIcon(new ImageIcon(imgMinijuegos));       
+        fondo.add(btnMinijuegos);
+        
+        //---------------- BOTON ESTADISTICAS ----------------
+        JButton btnLogIn = new JButton("ESTADISTICAS");
+        btnLogIn.setFont(new Font("Arial", Font.BOLD, 16));
+        btnLogIn.setBounds(820,560,285,65);
+        fondo.add(btnLogIn);
+        
+        //---------------- BOTON SALIR ----------------
+        JButton btnSalir = new JButton("SALIR");
+        btnSalir.setFont(new Font("Arial", Font.BOLD, 16));
+        btnSalir.setBounds(820, 680, 285, 65);
+        btnSalir.setIconTextGap(5);        
+        ImageIcon icoSalir = new ImageIcon(getClass().getResource("/utiles/salir.png"));
+        Image imgSalir = icoSalir.getImage().getScaledInstance(35, 35, Image.SCALE_SMOOTH);
+        btnSalir.setIcon(new ImageIcon(imgSalir));        
+        fondo.add(btnSalir);
+        
+        //---------------- MASCOTA ----------------
+        JLabel mascota = new JLabel();
+        ImageIcon mascotaIcon = new ImageIcon(getClass().getResource("/utiles/mascota3.png"));
+        Image mascotaEscalada = mascotaIcon.getImage().getScaledInstance(181, 264, Image.SCALE_SMOOTH);
+        mascota.setIcon(new ImageIcon(mascotaEscalada));
+        mascota.setBounds(485, 525, 181, 264); 
+        fondo.add(mascota);
+    }
+    
+}
+
